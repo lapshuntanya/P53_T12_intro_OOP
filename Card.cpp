2e12a8475b6b0e760e5e41035bbe0017a0a58cf5
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-06-24 17:40:29 +0300

    class Card

---
+++ Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tetiana Perederii on 24.06.2026.
+//
+
+#include "Card.h"
---
+++ Card.h
@@ -0,0 +1,25 @@
+//
+// Created by Tetiana Perederii on 24.06.2026.
+//
+
+#ifndef P53_T12_INTRO_OOP_CARD_H
+#define P53_T12_INTRO_OOP_CARD_H
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Card {
+    /*---------------- Поля (fields) ------------------------*/
+    unsigned long cardNumber;   //Номер картки
+    char* name;                 //Ім’я власника
+    char expiredDate[6];        //Дата дії
+    unsigned short PIN;         //Код картки
+    double cash;                //Сума грошей на картці
+
+
+};
+
+
+#endif //P53_T12_INTRO_OOP_CARD_H
