019dcddf2efebb60a5d0f84da584b2176703100d
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-06-24 18:48:19 +0300

    main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 int main() {
 
-    Card a1;
-     a1.
+    Card Daniil;
+    Card Daria("Zavizlova Daria", "06.46", 5000);
 
+    Card* Volodymyr = new Card();
+    Card* Marko = new Card("Kalashnikod Marko", "06.56", 2000);
+
+
+    Daniil.showInfo();
+    Daria.showInfo();
+
+    //(*Volodymyr).showInfo();
+    Volodymyr->showInfo();
+    Marko->showInfo();
+
+    //------------------ end
+    delete Volodymyr;
+    delete Marko;
 
     return 0;
 }
